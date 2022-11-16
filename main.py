f9ae5bb19bd6fff5929d7fd75839c38e8004fc04
mahdi <mmf1370py@gmail.com>
2022-11-16 06:41:18 -0800

    edit some variable names

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ window.title("This is my app")
 
 
 # Labels
-label_f = tk.Label(
+lbl_fahrenheit = tk.Label(
     master=window,
     text="Fahrenheit: ",
     height=3,
     width=15,
 )
-label_c = tk.Label(
+lbl_celsius = tk.Label(
     master=window,
     text="Celsius",
 )
-label_result = tk.Label(
+lbl_result = tk.Label(
     master=window,
     text="Result will be shown here...",
 )
 
 
 # Entries
-entry_f = tk.Entry(
+ent_fahrenheit = tk.Entry(
     master=window,
     width=40,
 )
@@ -34,13 +34,13 @@ entry_f = tk.Entry(
 
 def temp_calc():
     try:
-        label_result['text'] = (int(entry_f.get())-32) * (5/9)
+        lbl_result['text'] = (int(ent_fahrenheit.get())-32) * (5/9)
     except ValueError:
-        label_result['text'] = "You should enter a number!"
+        lbl_result['text'] = "You should enter a number!"
 
 
 # Buttons
-button_calc = tk.Button(
+btn_calc = tk.Button(
     master=window,
     text="Calc",
     width=8,
@@ -49,11 +49,11 @@ button_calc = tk.Button(
 
 
 # Show Widgets
-label_f.grid(row=0, column=0)
-entry_f.grid(row=0, column=1)
-button_calc.grid(row=0, column=2, padx=(20, 10))
-label_c.grid(row=1, column=0, pady=(2, 10))
-label_result.grid(row=1, column=1, pady=(2, 10))
+lbl_fahrenheit.grid(row=0, column=0)
+ent_fahrenheit.grid(row=0, column=1)
+btn_calc.grid(row=0, column=2, padx=(20, 10))
+lbl_celsius.grid(row=1, column=0, pady=(2, 10))
+lbl_result.grid(row=1, column=1, pady=(2, 10))
 
 
 # Run
